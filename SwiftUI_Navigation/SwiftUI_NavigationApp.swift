a7d2d6311d4fc585f561a79d46d5649dfcd8fdf8
mahfujulAppnap <36977885+mahfujulIslamAkash@users.noreply.github.com>
2024-03-15 02:23:19 +0600

    Initial Commit

---
+++ SwiftUI_Navigation/SwiftUI_NavigationApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_NavigationApp.swift
+//  SwiftUI_Navigation
+//
+//  Created by Appnap Mahfuj on 15/3/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_NavigationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
